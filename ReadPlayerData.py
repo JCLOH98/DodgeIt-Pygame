d174e4ec586fc6c5345dd2cc1c37f2bac2e435d7
JCLOH98 <jiachiewloh@gmail.com>
2019-06-22 17:04:28 +0800

    read the player data

--- ReadPlayerData.py
+++ ReadPlayerData.py
@@ -1,5 +1,17 @@
 
 def main():
     File = open('Data.dat','r')
+    Read = File.read()
+
+    #All the datas in Datas
+    Datas = Read.splitlines()
+    print(Datas)
+
+    #Show the set of datas
+    SetOfData = len(Read.splitlines())
+    print(SetOfData)
+
+    for i in range(SetOfData):
+        print(Datas[i].split(','))
 
 main()
